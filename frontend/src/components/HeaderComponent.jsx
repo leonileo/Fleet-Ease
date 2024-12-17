4f4acb9bacd2ba3f5841dbf3cf6de3d49a62b2b3
leonileo <leolittleprogrammer@gmail.com>
2024-12-17 05:04:30 +0300

    HeaderComponent

---
+++ frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+
+
+const HeaderComponent = () => {
+  return (
+    <div className="bg-[#1D3557] h-[57px] p-2 px-4 flex items-center justify-between text-white">
+        <h1 className="sm:text-2xl">FleetEase</h1>
+        <button className="button flex justify-center bg-[rgba(69,123,157,0.50)] p-1 px-8 rounded-full hover:bg-[#457B9D] transition-all">
+            <p>Manage vehicles now</p>
+        </button>
+    </div>
+  )
+}
+
+export default HeaderComponent
