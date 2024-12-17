207a645d0d2864a73c6c7bccb220c2bc5c08b7c8
leonileo <leolittleprogrammer@gmail.com>
2024-12-17 06:15:41 +0300

    Update: change the button into a link.

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -1,12 +1,14 @@
-
+import { Link } from 'react-router-dom'
 
 const HeaderComponent = () => {
   return (
     <div className="bg-[#1D3557] h-[57px] p-2 px-4 flex items-center justify-between text-white">
         <h1 className="sm:text-2xl">FleetEase</h1>
-        <button className="button flex justify-center bg-[rgba(69,123,157,0.50)] p-1 px-8 rounded-full hover:bg-[#457B9D] transition-all">
-            <p>Manage vehicles now</p>
-        </button>
+        <Link to={'/dashboard'}>
+          <button className="button flex justify-center bg-[rgba(69,123,157,0.50)] p-1 px-8 rounded-full hover:bg-[#457B9D] transition-all">
+              <p>Manage vehicles now</p>
+          </button>
+        </Link>
     </div>
   )
 }
