19aec9027f40edfe03db8a35e098466a60bc2f02
leonileo <leolittleprogrammer@gmail.com>
2024-12-17 15:56:13 +0300

    HomeScreen

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,19 @@
+import CtaComponent from "../components/CtaComponent"
+import FeaturesComponent from "../components/FeaturesComponent"
+import HeaderComponent from "../components/HeaderComponent"
+
+const HomeScreen = () => {
+  return (
+    <div>
+        <HeaderComponent />
+        <div className="bottom sm:flex-row flex flex-col-reverse w-full sm:justify-center sm:h-[95vh] h-auto">
+            {/* left side */}
+            <FeaturesComponent />
+            {/* right side */}
+            <CtaComponent />
+        </div>
+    </div>
+  )
+}
+
+export default HomeScreen
