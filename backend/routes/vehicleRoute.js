49fd6bbf1b1f3f06642cd68026d4bc15a9d569f2
leonileo <leolittleprogrammer@gmail.com>
2024-12-19 02:04:56 +0300

    Update: added routes for the vehicle crud operation and dashboard.

---
+++ backend/routes/vehicleRoute.js
@@ -0,0 +1,15 @@
+// API ROUTE and FUNCTION
+// vehicleRoute.js
+// Import necessary modules
+const express = require('express');
+const { dashboard, addVehicle, updateVehicle, removeVehicle } = require('../controllers/vehicleController')
+
+const router = express.Router();
+
+router.get('/', dashboard);
+router.post('/add-vehicle', addVehicle);
+router.route('/:vehicleID/')
+.put(updateVehicle)
+.delete(removeVehicle);
+
+module.exports = router;
