776eef858524ccb5030472d5e7bb952b42ee06ff
leonileo <leolittleprogrammer@gmail.com>
2024-12-19 02:04:04 +0300

    Updated: added the vehicle model.

--- backend/models/vehicleModel.js
+++ backend/models/vehicleModel.js
@@ -1,1 +1,18 @@
+// DATA SCHEMA
 // vehicleModel.js
+// Import necessary modules
+const mongoose = require('mongoose');
+
+const vehicleSchema = new mongoose.Schema({
+    vehicleName: {type: String, required: true, trim: true},
+    vehicleType: {type: String, required: true, enum: ['Car', 'Truck', 'Van', 'Bus', 'Motorbike']},
+    licensePlate: {type: String, unique: true, required: true, trim: true},
+    vehicleMileage: {type: Number, required: true, trim: true},
+    vehicleStatus: {type: String, required: true},
+    vehiclePicture: {type: String, required: true}, 
+    vehicleRegesterationDate: {type: Date, default: Date.now},
+    vehicleLastUpdatedAt: {type: Date, default: Date.now},
+}, { timestamps: true })
+
+const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+module.exports = Vehicle;
