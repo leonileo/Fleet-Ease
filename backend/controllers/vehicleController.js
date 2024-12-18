826b1ae9bad01b2703411392a0362f0ecddb8c85
leonileo <leolittleprogrammer@gmail.com>
2024-12-19 02:07:17 +0300

    Update: added the basic structure for the vehicle controller function.

--- backend/controllers/vehicleController.js
+++ backend/controllers/vehicleController.js
@@ -1,1 +1,34 @@
 // vehicleController.js
+const asyncHandler = require('../middleware/asyncHandler.js');
+
+const dashboard = asyncHandler( async (req, res) => {
+    // Dashboard function
+    res.json("Dashboard")
+});
+
+const addVehicle = asyncHandler( async (req, res) => {
+    // addVehicle function
+    res.json("addVehicle")
+ 
+});
+
+const updateVehicle = asyncHandler( async (req, res) => {
+    // updateVehicle function
+    res.json("updateVehicle")
+ 
+});
+
+const removeVehicle = asyncHandler( async (req, res) => {
+    // removeVehicle function
+    res.json("removeVehicle")
+ 
+});
+
+
+
+module.exports = {
+    dashboard,
+    addVehicle,
+    updateVehicle,
+    removeVehicle
+}
