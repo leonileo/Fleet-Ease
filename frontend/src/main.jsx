8b1cb546a99c66bece37ef2b87d675158383cd19
leonileo <leolittleprogrammer@gmail.com>
2024-12-18 05:23:16 +0300

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen.jsx';
+import DashboardScreen from './screens/Admin/DashboardScreen.jsx';
+import AddVehicle from './screens/Admin/AddVehicle.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />} >
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/dashboard' element={<DashboardScreen />} />
+      <Route path='/dashboard/add-vehicle' element={<AddVehicle />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
