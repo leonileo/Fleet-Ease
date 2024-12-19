3b365fb136517fc9c52b9b8aea1a385acb18aa85
leonileo <leolittleprogrammer@gmail.com>
2024-12-19 04:06:47 +0300

    Update: updated the upload function by adding coudinary storage configuration, creating the post function.

---
+++ backend/routes/uploadRoute.js
@@ -0,0 +1,40 @@
+// API ROUTE and FUNCTION
+// uploadRoutes.js
+// Import necessary modules
+const express = require('express');
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
+
+const router = express.Router();
+
+// main function
+
+cloudinary.config({
+  cloud_name: process.env.cloudinary_name,
+  api_key: process.env.cloudinary_api_key,
+  api_secret: process.env.cloudinary_api_secret,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'FleetEase', // Folder name in Cloudinary
+    allowed_formats: ['jpg', 'png', 'jpeg'], // Allowed image formats
+  },
+});
+
+const upload = multer({ storage });
+
+router.post('/', upload.single('image'), (req, res) => {
+  try {
+    res.status(200).json({
+      message: 'Image uploaded successfully!',
+      imageUrl: req.file.path, // Cloudinary URL
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Image upload failed', error });
+  }
+});
+
+module.exports = router;
