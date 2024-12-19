0c0958f90f2c9fa88f9d6caca4114f608eafbfbb
leonileo <leolittleprogrammer@gmail.com>
2024-12-19 03:59:56 +0300

    Update: updated the field 'vehicleStatus' by adding enum values.

--- backend/models/vehicleModel.js
+++ backend/models/vehicleModel.js
@@ -8,7 +8,7 @@ const vehicleSchema = new mongoose.Schema({
     vehicleType: {type: String, required: true, enum: ['Car', 'Truck', 'Van', 'Bus', 'Motorbike']},
     licensePlate: {type: String, unique: true, required: true, trim: true},
     vehicleMileage: {type: Number, required: true, trim: true},
-    vehicleStatus: {type: String, required: true},
+    vehicleStatus: {type: String, required: true, enum: ['Active', 'Maintenance', 'Inactive', 'In Transit'],},
     vehiclePicture: {type: String, required: true}, 
     vehicleRegesterationDate: {type: Date, default: Date.now},
     vehicleLastUpdatedAt: {type: Date, default: Date.now},
