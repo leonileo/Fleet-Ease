7603b0f6f0664a0dc68570dbd872ffe53462de7e
leonileo <leolittleprogrammer@gmail.com>
2024-12-20 11:02:25 +0300

    SkeletonTable

---
+++ frontend/src/components/SkeletonTable.jsx
@@ -0,0 +1,108 @@
+
+const SkeletonTable = () => {
+  return (
+    <div className="table-skeleton">
+        <div className="theader w-full bg-[rgba(0,0,0,0.8)] sm:gap-5 gap-1 p-4 h-[5vh] flex items-center">
+            <div className="animate-pulse duration-75 flex justify-center w-full">
+                <span className="sm:w-[50%] w-full bg-[rgba(255,255,255,.9)] rounded sm:rounded-full h-5 block"></span>
+            </div>
+            <div className="animate-pulse duration-150 flex justify-center w-full">
+                <span className="sm:w-[50%] w-full bg-[rgba(255,255,255,.9)] rounded sm:rounded-full h-5 block"></span>
+            </div>
+            <div className="animate-pulse duration-300 flex justify-center w-full">
+                <span className="sm:w-[50%] w-full bg-[rgba(255,255,255,.9)] rounded sm:rounded-full h-5 block"></span>
+            </div>
+            <div className="animate-pulse duration-500 flex justify-center w-full">
+                <span className="sm:w-[50%] w-full bg-[rgba(255,255,255,.9)] rounded sm:rounded-full h-5 block"></span>
+            </div>
+            <div className="animate-pulse duration-1000 flex justify-center w-full">
+                <span className="sm:w-[50%] w-full bg-[rgba(255,255,255,.9)] rounded sm:rounded-full h-5 block"></span>
+            </div>
+        </div>
+        <div className="trows">
+            <div className="trow even bg-[rgba(0,0,0,0.3)] animate-pulse duration-1000 h-[5vh ] p-4 flex items-center sm:gap-5 gap-1">
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <div className="w-full flex gap-4 p-4">
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                </div>
+            </div>
+            <div className="trow odd bg-[rgba(0,0,0,0.5)] animate-pulse p-4 flex items-center sm:gap-5 gap-1">
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <div className="w-full flex gap-4 p-4">
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                </div>
+            </div>
+            <div className="trow even bg-[rgba(0,0,0,0.3)] animate-pulse duration-1000 h-[5vh ] p-4 flex items-center sm:gap-5 gap-1">
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <div className="w-full flex gap-4 p-4">
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                </div>
+            </div>
+            <div className="trow odd bg-[rgba(0,0,0,0.5)] animate-pulse p-4 flex items-center sm:gap-5 gap-1">
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <div className="w-full flex gap-4 p-4">
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                </div>
+            </div>
+            <div className="trow even bg-[rgba(0,0,0,0.3)] animate-pulse duration-1000 h-[5vh ] p-4 flex items-center sm:gap-5 gap-1">
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <div className="w-full flex gap-4 p-4">
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                </div>
+            </div>
+            <div className="trow odd bg-[rgba(0,0,0,0.5)] animate-pulse p-4 flex items-center sm:gap-5 gap-1">
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <div className="w-full flex gap-4 p-4">
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                </div>
+            </div>
+            <div className="trow even bg-[rgba(0,0,0,0.3)] animate-pulse duration-1000 h-[5vh ] p-4 flex items-center sm:gap-5 gap-1">
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <div className="w-full flex gap-4 p-4">
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                </div>
+            </div>
+            <div className="trow odd bg-[rgba(0,0,0,0.5)] animate-pulse p-4 flex items-center sm:gap-5 gap-1">
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                <div className="w-full flex gap-4 p-4">
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                    <span className="w-full bg-white block h-4 rounded sm:rounded-full"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SkeletonTable
