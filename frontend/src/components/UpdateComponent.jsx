ba7d8ca44c53339adc50187c261826bce5ddc3da
leonileo <leolittleprogrammer@gmail.com>
2024-12-20 18:22:37 +0300

    Update: integrated the component with the backend function.

--- frontend/src/components/UpdateComponent.jsx
+++ frontend/src/components/UpdateComponent.jsx
@@ -2,6 +2,10 @@
 import { useState } from "react"
 import { FaCircleInfo } from "react-icons/fa6"
 import { useDropzone } from 'react-dropzone'
+import { useUploadImageMutation, useUpdateVehicleMutation } from '../slices/vehicleSlice'
+import { toast } from "sonner"
+import { useNavigate } from "react-router-dom"
+import { Spinner } from "flowbite-react"
 
 const UpdateComponent = ({ vehicle, setUpdateModal }) => {
   // states
@@ -11,8 +15,13 @@ const UpdateComponent = ({ vehicle, setUpdateModal }) => {
   const [vStatus, setVStatus] = useState(vehicle.vehicleStatus)
   const [vMileage, setVMileage] = useState(vehicle.vehicleMileage)
   const [image, setImage] = useState(null);
+  const [file, setFile] = useState(null);
+  const [uploaded, setUploaded] = useState(true);
 
-  
+  const [uploadImage, { isLoading: uploadLoader}] = useUploadImageMutation();
+  const [updateVehicle, { isLoading }] = useUpdateVehicleMutation();
+  const navigate = useNavigate();
+
   // dummy data
   const types = [
     {type: "Car"},
@@ -25,8 +34,10 @@ const UpdateComponent = ({ vehicle, setUpdateModal }) => {
   // Function for drag and drop image.
   const onDrop = (acceptedFiles) => {
     if(acceptedFiles.length > 0) {
-      const file = acceptedFiles[0];
-      setImage(Object.assign(file, { preview: URL.createObjectURL(file) }))
+      const file1 = acceptedFiles[0];
+      setFile(file1);
+      setUploaded(false);
+      setImage(Object.assign(file1, { preview: URL.createObjectURL(file1) }))
     }
   }
 
@@ -39,23 +50,56 @@ const UpdateComponent = ({ vehicle, setUpdateModal }) => {
   // Function for drag and drop image.
 
   // Function to submit the form
-  const handleUpdate = (e) => {
+  const handleUpdate = async (e) => {
     e.preventDefault();
 
-    alert(`
-      Name: ${vName}
-      LicensePlate: ${vPlate}
-      Type: ${vType}
-      Status: ${vStatus}
-      Mileage: ${vMileage}
-      Vehicle picture: ${image ? image : vehicle.vehiclePicture}
-      `)
+    try {
+      const formData = new FormData();
+      formData.append('image', file);
+
+      try {
+        // this try/catch statment has 2 function, 
+        // that uploads the image then set the image url
+        // and update the vehicle info.
+        // the if statement checks if the image is uploaded to cloudinary already or not.
+
+        let res, imageUrl;
+        if (!uploaded) {
+          res = !uploaded && await uploadImage(formData).unwrap();
+          imageUrl = res.imageUrl;
+          setUploaded(true);
+          toast.success(res.message);
+        }
+
+        const response = await updateVehicle({
+          id: vehicle._id,
+          vehicleName: vName,
+          vehicleType: vType,
+          licensePlate: vPlate,
+          vehicleMileage: Number(vMileage),
+          vehicleStatus: vStatus,
+          vehiclePicture: imageUrl ? imageUrl : vehicle.vehiclePicture
+        }).unwrap();
+  
+        toast.success(response.message)
+        setTimeout(() => {
+          navigate('/dashboard')
+          location.reload()
+        }, 2500)
+
+      } catch (error) {
+        toast.error(error?.data?.message || error?.error)
+      }
+      
+    } catch (error) {
+        toast.error(error?.data?.message || error?.error)
+    }
   }
-    
+
   return (
     <>
       {/* dashboard content */}
-      <div className="dashboard-content overflow-auto md:w-full w-[88%] sm:p-10 p-5 space-y-10">
+      <div className="dashboard-content overflow-auto md:w-full sm:p-10 xl:p-5 p-2 space-y-10">
         <form onSubmit={handleUpdate} className="form bg-[#F1FAEE] text-black min-h-[70vh] border rounded p-5 md:px-10">
           <div className="info flex items-center gap-5 text-[#1D3557]">
             <FaCircleInfo />
@@ -65,9 +109,10 @@ const UpdateComponent = ({ vehicle, setUpdateModal }) => {
            {/* upload image */}
           <div className="picture flex justify-center items-center my-5">
             <div 
-            className="xl:w-[40%] xl:h-[400px] h-[250px] text-center grid justify-center items-end bg-white p-4 border-2 border-dashed rounded-md cursor-pointer hover:border-[#1D3557] text-[#1D3557] transition-all">
+            className="xl:w-[40%] xl:h-[400px] h-[250px] text-center grid justify-center items-end bg-white xl:p-4 p-2 border-2 border-dashed overflow-hidden rounded-md cursor-pointer hover:border-[#1D3557] text-[#1D3557] transition-all">
               <input {...getInputProps()} />
               {image ? (
+
                 <div className="xl:w-[500px] w-full flex items-center h-full overflow-hidden rounded"
                 {...getRootProps()}
                 >
@@ -76,18 +121,18 @@ const UpdateComponent = ({ vehicle, setUpdateModal }) => {
                     alt="Preview of uploaded picture"
                     className="mr-2 rounded"
                   />
-                  <p>sdsd</p>
                 </div>
+                
               ) : (
-                <div {...getRootProps()}>
-                  <div className="xl:w-[500px] w-full flex items-center h-full overflow-hidden rounded">
+                <div {...getRootProps()} className="h-full p-2 grid">
+                  <div className="xl:w-[500px] w-full h-full flex items-center justify-center rounded">
                     <img 
                       src={vehicle.vehiclePicture}
                       alt={`${vName} picture"`}
                       className="mr-2 rounded"
                     />
                   </div>
-                  <div className="btn flex justify-end my-2">
+                  <div className="btn flex justify-end my-2 items-end">
                     <button type="button" className="bg-[#1D3557] hover:bg-white hover:border-red-50 border border-transparent hover:text-[#1D3557] font-semibold text-white transition-all px-5 p-2 rounded-full">Update picture</button>
                   </div>
                 </div>
@@ -134,8 +179,9 @@ const UpdateComponent = ({ vehicle, setUpdateModal }) => {
                 <label htmlFor="vStatus" className="w-[200px]">Vehicle status <span className="text-red-600 font-bold">*</span></label>
                 <select value={vStatus} onChange={(e) => setVStatus(e.target.value)} name="status" id="vStatus" className="bg-[#457B9D] w-full text-white p-3 border rounded">
                   <option value="Active">Active</option>
-                  <option value="In active">In active</option>
                   <option value="Maintenance">Maintenance</option>
+                  <option value="Inactive">Inactive</option>
+                  <option value="In Transit">In Transit</option>
                 </select>
               </div>
 
@@ -147,12 +193,15 @@ const UpdateComponent = ({ vehicle, setUpdateModal }) => {
               
             </div>
 
-            <div className="btns flex gap-5">
+            <div className="btns flex xl:gap-5 gap-2">
               {/* update button */}
-              <button type="submit" className="my-5 px-16 p-2 border border-transparent hover:border-[#1D3557] hover:bg-transparent hover:text-[#1D3557] transition-all hover: bg-[#1D3557] text-white font-semibold rounded-full">Update vehicle</button>
+              {isLoading || uploadLoader 
+              ? <button disabled className="xl:my-5 xl:px-16 px-0 xl:w-auto w-full p-2 border border-transparent hover:border-[#1D3557] hover:bg-transparent hover:text-[#1D3557] transition-all bg-[#1D3557] text-white font-semibold rounded-full flex items-center gap-3">Updateing ... <Spinner className="w-5 text-[#457B9D] fill-[#1D3557]" /></button>
+              : <button type="submit" className="xl:my-5 xl:px-16 px-0 xl:w-auto w-full p-2 border border-transparent hover:border-[#1D3557] hover:bg-transparent hover:text-[#1D3557] transition-all bg-[#1D3557] text-white font-semibold rounded-full">Update vehicle</button>
+              }
 
               {/* Cancle button */}
-              <button type="button" onClick={() => setUpdateModal(false)} className="my-5 px-10 p-2 border border-transparent hover:border-red-400 hover:bg-transparent hover:text-red-400 transition-all hover: bg-red-400 text-white font-semibold rounded-full">Cancle</button>
+              <button type="button" onClick={() => setUpdateModal(false)} className="xl:my-5 xl:px-10 px-0 xl:w-auto w-full p-2 border border-transparent hover:border-red-400 hover:bg-transparent hover:text-red-400 transition-all hover: bg-red-400 text-white font-semibold rounded-full">Cancle</button>
             </div>
 
           </div>
