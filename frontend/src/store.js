896560778ee0d977270791b09e06504093242ca8
leonileo <leolittleprogrammer@gmail.com>
2024-12-20 11:04:16 +0300

    Redux store

---
+++ frontend/src/store.js
@@ -0,0 +1,14 @@
+// store.js
+// Import necessary modules
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
+
+const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
+});
+
+export default store;
