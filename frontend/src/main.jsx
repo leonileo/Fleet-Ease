7e3e87dc68bd6c0783de30aff38cc7d75cf8d9b7
leonileo <leolittleprogrammer@gmail.com>
2024-12-20 11:09:43 +0300

    Update: configured the redux provide.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
+// index.css
+// Import necessary modules
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store';
+
+// components
 import HomeScreen from './screens/HomeScreen.jsx';
 import DashboardScreen from './screens/Admin/DashboardScreen.jsx';
 import AddVehicle from './screens/Admin/AddVehicle.jsx';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,6 +26,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
